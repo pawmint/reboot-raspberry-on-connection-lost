3e07c275638996e6c7786084793cd4b03f00417c
Romain Endelin <romain.endelin@gmail.com>
2015-04-24 16:26:19 +0200

    Added setup.py

--- reboot_on_connection_lost/main.py
+++ reboot_on_connection_lost/main.py
@@ -1,6 +1,8 @@
 import requests
 import os
 import time
+import sys
+from threading import Timer
 
 
 def check_connection(url='8.8.8.8', timeout=5):
@@ -17,12 +19,19 @@ def shutdown():
 
 
 def main():
+    is_timer_running = False
+    timer = None
+    if len(sys.argv) == 2:
+        timer_duration = int(sys.argv[1])
+    else:
+        timer_duration = 3600
+
     for has_connection in check_connection():
         if has_connection and is_timer_running:
             timer.cancel()
             is_timer_running = False
         elif not (has_connection or is_timer_running):
-            timer = Timer(3600, shutdown)
+            timer = Timer(timer_duration, shutdown)
             timer.start()
             is_timer_running = True
         time.sleep(10)
---
+++ setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+
+readme = open('README.md').read()
+
+setup(
+    name='RebootRaspberryOnConnectionLost',
+    version='0.1',
+    description=('Reboots the raspberry if the connection was lost '
+                 'for too long')
+    long_description=readme,
+    author='Romain Endelin',
+    author_email='romain.endelin@mines-telecom.fr',
+    url='https://github.com/pawmint/reboot-raspberry-on-connection-lost',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests>=2.6.2'
+    ],
+    entry_points = {
+        'console_scripts': ['reboot_on_collection_lost=reboot_on_collection_lost.main:main'],
+    },
+    license='Copyright',
+    zip_safe=True,  # To be verified
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Environment :: Console',
+        'License :: Other/Proprietary License',
+        'Topic :: Scientific/Engineering',
+    ],
+)
