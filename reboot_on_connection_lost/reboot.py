3e66357bd15ade22bb94154fba0e3f08a06ae4fe
Fuzzy Anastazia <fuzzyprincesse@gmail.com>
2015-05-26 17:10:58 +0200

    make sure that shutdown directory exists

--- reboot_on_connection_lost/reboot.py
+++ reboot_on_connection_lost/reboot.py
@@ -4,6 +4,7 @@ import time
 from datetime import datetime
 import sys
 from threading import Timer
+from appdirs import user_log_dir
 
 
 def check_connection(url='http://www.google.com', timeout=5):
@@ -15,13 +16,20 @@ def check_connection(url='http://www.google.com', timeout=5):
             yield False
 
 
+def ensure_shutdown_dir():
+    try:
+        os.stat(user_log_dir('reboot_on_connexion_lost', 'pawmint'))
+    except:
+        os.makedirs(user_log_dir('reboot_on_connexion_lost', 'pawmint'))
+
 def shutdown():
-    with open("shutdown.txt", "a") as myfile:
+    with open(os.path.join(user_log_dir("reboot_on_connexion_lost", "pawmint"), "shutdown.txt"), "a") as myfile:
         myfile.write(datetime.now().strftime('%Y%m%d_%H%M\n'))
-        os.system("shutdown now -r")
+    os.system("shutdown now -r")
 
 
 def main():
+    ensure_shutdown_dir()
     is_timer_running = False
     timer = None
     TIMER_DURATION = 3600
